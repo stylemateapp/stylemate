33e68ffe958a298a18f260816df7b7efee4c431d
Egor <egorsmir@gmail.com>
2013-07-30 02:57:58 +0400

    Change Celsius to Fahrenheit

--- frontend/app/scripts/services/weather.js
+++ frontend/app/scripts/services/weather.js
@@ -11,7 +11,7 @@ app.factory('WeatherService', ['serverUrl', function (serverUrl) {
 
     ForecastIO.prototype.requestData = function (latitude, longitude) {
 
-        var request_url = this.PROXY_SCRIPT + '?url=' + latitude + ',' + longitude + '?units=auto';
+        var request_url = this.PROXY_SCRIPT + '?url=' + latitude + ',' + longitude + '?units=us';
         var xhr = new XMLHttpRequest();
         var content;
         xhr.onreadystatechange = function () {
