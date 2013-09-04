82878f3b7e5a09e9ce9a472ff37f7a9f37898e09
Egor <egorsmir@gmail.com>
2013-09-04 18:47:57 +0400

    Preloading of angular view templates on application start and some other improvements

--- protected/views/frontend/site/index.php
+++ protected/views/frontend/site/index.php
@@ -1,12 +1,1 @@
-<?php
-    /**
-     * @var Controller $this
-     */
-?>
-
-<?php $this->pageTitle = Yii::app()->name; ?>
-
-<div>
-    <?php echo CHtml::link('Go to admin panel', $this->createUrl('admin/index')); ?> <br>
-    <?php echo CHtml::link('Logout', $this->createUrl('site/logout')); ?>
-</div>
+Stylemateapp.com
