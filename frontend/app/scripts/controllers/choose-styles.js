aa5703e74372afee582502e531fe63621d32ddea
Egor <egorsmir@gmail.com>
2013-09-09 13:19:06 +0400

    Choose Styles Markup

--- frontend/app/scripts/controllers/choose-styles.js
+++ frontend/app/scripts/controllers/choose-styles.js
@@ -1,9 +1,17 @@
 'use strict';
 
-function ChooseStylesController($scope, $http,  $state, serverUrl) {
+function ChooseStylesController($scope, $http, $state, serverUrl) {
 
     $scope.selectedStyles = [];
     $scope.errorMessage = '';
+
+    $scope.$watch('errorMessage', function() {
+
+        if($scope.errorMessage != '') {
+
+            alert($scope.errorMessage);
+        }
+    });
 
     $http.get(serverUrl + '/user/getStyles')
 
