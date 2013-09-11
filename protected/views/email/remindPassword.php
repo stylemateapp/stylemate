cee8754715bbd7f62d2235e9959e730610bf0be8
Egor <egorsmir@gmail.com>
2013-09-11 08:11:22 +0400

    remindpassword template change

--- protected/views/email/remindPassword.php
+++ protected/views/email/remindPassword.php
@@ -4,8 +4,6 @@
 
 <p>You password is <strong><?php echo $password; ?></strong></p>
 
-<p><a href="#">Return to Stylemate application by clicking this link</a></p>
-
 <p>If you didn't request a password remind, you can ignore this message -- someone probably typed in your username or email address by accident.</p>
 
 <p>Thank You!</p>
